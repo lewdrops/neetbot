f0e3aabc63b8ac33640e85bb262d31eebd83bf9e
Debo Boko <qedpie@gmail.com>
2019-02-07 14:42:26 -0500

    add comment

--- chitchat.py
+++ chitchat.py
@@ -2,12 +2,12 @@ import random
 
 import asyncio
 
-GOOD_BOT_REPLIES = [_("That's what you are, but what am I?"),
+GOOD_BOT_REPLIES = [_("That's what you are, but what am I?"),  # these replies are going to be defined only once, in one language
                     _("No, I am Great Bot"),
                     _("B-baka!")]
 
 
-async def send_message(message, text=_("Hello!"), delete_in=False): #Note: this translation will be evaluated at initialization
+async def send_message(message, text=_("Hello!"), delete_in=False):  # Note: this translation will be evaluated at initialization
     print(_("received a message"), '-', message.content, '\n', message)
 
     msg = await message.channel.send(text)
